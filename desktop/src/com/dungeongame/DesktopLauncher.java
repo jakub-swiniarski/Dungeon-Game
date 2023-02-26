6f67715d5d7af492874f93364eb7812ac54bca9a
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-02-26 17:02:34 +0100

    vsync

--- desktop/src/com/dungeongame/DesktopLauncher.java
+++ desktop/src/com/dungeongame/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.dungeongame.DungeonGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(75);
+		config.useVsync(true);
 		config.setWindowedMode(1280,720);
 		config.setTitle("Dungeon Game");
 		new Lwjgl3Application(new DungeonGame(), config);
